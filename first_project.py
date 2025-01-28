acc0923ab3480b8f632449f273f5e286d160af9f
Sdzd07 <97201149+Sdzd07@users.noreply.github.com>
2025-01-28 17:45:16 +0100

    Update first_project.py

--- first_project.py
+++ first_project.py
@@ -3,7 +3,7 @@ full_name = input("Enter your full name please: ").lower().title()
 # change input(full_name) to the list 
 name_lst = full_name.split()
 # greeting the user by his&her first name on our company and store it on variable (message)
-message = f'Hello, {name_lst[0]}!\nWellcome at Saliho Company!'
+message = f'Hello, {name_lst[0]}!\nWellcome at Hunter Company!'
 #print the variable (message) 
 print("_"*50)
 print(message)
