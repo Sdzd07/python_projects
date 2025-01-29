41dd2e04169586865d1fe4820214a9c10cf98e5c
Salah <97201149+Sdzd07@users.noreply.github.com>
2025-01-29 17:26:18 +0100

    Update program_analyse_salaries.py

--- program_analyse_salaries.py
+++ program_analyse_salaries.py
@@ -1,4 +1,4 @@
-#this script analyse the salary on the list and give the user the greater&smaller and the medium of salary
+#this script analyse the salary on the list and give the user the Maximum&Minimum and the Average of salary
 #first step ask user for input all the salaries,or we can put early list.
 salaries = input("Enter more than one salary and separeat between salaries by comma please : ")
 comma = ","
@@ -10,14 +10,14 @@ else:
     #first should be change the items on the list from string data type to float data types by using map method 
     # or with while (for loops)
      float_salaries = list(map(float,list_salariess))
-    #analyse the greater salary in the list by using max method 
+    #analyse the maximum salary in the list by using max method 
      max_salary = max(float_salaries)
-     print(f"The greater salary is: {max_salary} $")
-    #analyse the smaller salary in the list by using max method 
+     print(f"The maximum salary is: {max_salary} $")
+    #analyse the minimum salary in the list by using max method 
      min_salary = min(float_salaries)
-     print(f"The smaller salary is: {min_salary} $")
+     print(f"The minimum salary is: {min_salary} $")
     #for know the medium salary from the list we should addition all the items on the list together
     # and division the total on number of items have on the list (salaries)
      sum_salarys = sum(float_salaries)
-     medium_salary = sum_salarys/len(float_salaries)
-     print(f"The medium salary is: {medium_salary} $")
+     average_salary = sum_salarys/len(float_salaries)
+     print(f"The average salary is: {medium_salary} $")
