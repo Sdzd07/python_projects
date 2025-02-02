828de41ef0fdb78209744ad4dbfc6c6ac513596a
Salah <97201149+Sdzd07@users.noreply.github.com>
2025-02-02 18:58:55 +0100

    Add files via upload

---
+++ project_roll dice.py
@@ -0,0 +1,13 @@
+#program for guess Roll Dice 
+#import random module 
+import random 
+roll_dice = random.randint(1,6)
+gusse = int(input("Enter number between 1 to 6 :"))
+print("-"*20)
+print(f"The Roll Dice is {roll_dice}")
+#check the answer of user
+if gusse == roll_dice:
+    print("You won!")
+else: 
+    print("You lost")    
+   
