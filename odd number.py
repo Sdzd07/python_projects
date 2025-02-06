ff351875390d7106574362f4ab8153ce3e869ba7
Salah <97201149+Sdzd07@users.noreply.github.com>
2025-02-06 22:58:15 +0100

    Add files via upload

---
+++ odd number.py
@@ -0,0 +1,6 @@
+#program print the negative odd numbers between -100 to 100.
+negative_number = -100
+while negative_number<0:
+    if negative_number%2 != 0:  
+        print(negative_number)
+    negative_number +=1
