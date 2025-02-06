c648802843e3d5cff7c58c0849abdb238588f1b6
Salah <97201149+Sdzd07@users.noreply.github.com>
2025-02-06 22:48:17 +0100

    Add files via upload

---
+++ list&while.py
@@ -0,0 +1,8 @@
+#program add the numbers from 12 to 99 to a list then print the list. 
+#put variable number
+list_number = []
+number = 12
+while number<100:
+     list_number.append(number)
+     number +=1
+print(list_number)
