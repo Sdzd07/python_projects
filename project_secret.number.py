932400444ff92f5ff15a90fc69834fe95ba78b4f
Salah <97201149+Sdzd07@users.noreply.github.com>
2025-02-06 22:15:38 +0100

    Update project_secret.number.py

--- project_secret.number.py
+++ project_secret.number.py
@@ -11,7 +11,7 @@ won_numbers = [secret_number1, secret_number2, secret_number3, secret_number4]
 guess = int(input("Enter number between 1 to 20 :"))
 #check the input if is correctly & print result
 if 1>guess or guess>20:
-    print("Please enter number between 1 to 20: ")
+    input("Please enter number between 1 to 20: ")
 elif guess in won_numbers:
     print("Bravooo you won!")
 else: 
