95693089e2e36c4c2989fac795ef2085f750111e
Salah <97201149+Sdzd07@users.noreply.github.com>
2025-02-07 14:41:05 +0100

    Add files via upload

---
+++ totalnums&while.py
@@ -0,0 +1,19 @@
+#Print the total of the numbers that the user will Enter. 
+#get input from user 
+total_nums = []
+while True:
+    number = input("Enter the number and when you finish write done: ")     
+    total_nums.append(number)
+    if number=="done".lower() or number=="done".upper():
+        break
+    print(sum(map(int,total_nums)))
+
+#solution by another way
+print("_"*50) 
+total_nums = 0
+while True:
+    number = input("Enter the number when you finish write done : ")     
+    if number=="done".lower() or number=="done".upper():
+        break
+    print(total_nums+int(number))
+   
