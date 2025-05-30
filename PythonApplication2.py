2079ca1f9b85cb41a68778c027ad99af8ae6c8b0
Flaga <97201149+Sdzd07@users.noreply.github.com>
2025-05-30 20:01:18 +0200

    Add files via upload

---
+++ PythonApplication2.py
@@ -0,0 +1,89 @@
+#print the pizza name and price in the follwing info pizzas :
+#pizza to print:
+#Pepperonie pizza 
+#chicken pizza 
+#Hawiian pizza
+pizzas = {
+    "Pepperonie Pizza": 100, 
+    "Margherita Pizza": 90, 
+    "Meet Lover": 80, 
+    "Chicken Pizza": 70, 
+    "cheese": 60, 
+    "Veggie": 40, 
+    "Hawiian": 90 
+    }
+#initiliaze for loop for print all the pizzas and price 
+print("Pepperonie Pizza cost : ", pizzas["Pepperonie Pizza"])
+print("Chicken pizza cost :" ,pizzas["Chicken Pizza"])
+#----------------------------------------------------------
+#print the follwing drinks from the menu 
+#Drinks to print:
+#Coke 
+#Mango Juice
+#Tea 
+#Coffee
+drinks = {
+    "Coke": 70, 
+    "Mango Juice": 90, 
+    "Tea": 30, 
+    "Coffee": 20, 
+    "Orange Juice": 80,
+    "Pepsi": 90 
+}
+print("Coke drinks cost :" ,drinks["Coke"])
+print("Mango Juice cost :" ,drinks["Mango Juice"])
+print("Tea cost :" ,drinks["Tea"])
+print("Coffee cost :" ,drinks["Coffee"])
+print("_"*40)
+#--------------------------------------------------
+#add the follwing desertes items in the resturent menu 
+#Ice cram - 70
+#Chocolate Cacke - 100
+#Cheese Cacke - 80
+#Brownie - 60
+#Donuit - 50
+menu = {
+    "Cheese Pizza": 120, 
+    "Viggie Pizza": 90, 
+    "Hawaiian Pizza": 70, 
+    "Coke": 40, "Sprite": 30, 
+    "Fanta": 25
+}
+#make dictionary to add  the items 
+new_items = {
+    "Ice cram" :70,
+    "Chocolate Cacke":100,
+    "Cheese Cacke": 80,
+    "Brownie" : 60,
+    "Donuit" : 50
+}
+#initiliaz for loop for add items in the new dect named new_menu
+for item in new_items:
+    menu[item] = new_items[item]
+del(new_items)
+print(menu)
+#print(new_desertes)
+print("_"*40)
+#----------------------------------------------------------
+#another solution 
+desertes = {
+    "Cheese Pizza": 120, 
+    "Viggie Pizza": 90, 
+    "Hawaiian Pizza": 70, 
+    "Coke": 40, "Sprite": 30, 
+    "Fanta": 25
+}
+#make new dictionary to add new item on it 
+new_desertes = {
+    "Ice cram" :70,
+    "Chocolate Cacke":100,
+    "Cheese Cacke": 80,
+    "Brownie" : 60,
+    "Donuit" : 50
+}
+#add the new deictionary in the origenal dictonary(desertes)
+desertes.update(new_desertes)
+print(desertes)
+del(new_desertes)
+#print(new_desertes)
+print("_"*40)
